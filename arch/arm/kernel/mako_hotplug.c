74c13b307181b3d2b7721f9967b0a5a2ef3ed921
n3ocort3x <root@n3ocort3x-tuxbook.(none)>
2013-06-08 20:33:53 +0200

    cpufreq: interactive: add tunable to enable/disable dynamic_scaling of the governor tunables. I had to add some hooks around where I made to scale

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -84,14 +84,12 @@ static void first_level_work_check(unsigned long now)
 
     for_each_possible_cpu(cpu)
     {
-        if (cpu && !cpu_online(cpu))
+        if (cpu && likely(!cpu_online(cpu)))
         {
             cpu_up(cpu);
             pr_info("Hotplug: cpu%d is up - high load\n", cpu);
         }
     }
-
-    scale_interactive_tunables(0, 80, 10, 80);
 
     stats.time_stamp = now;
 }
@@ -106,19 +104,13 @@ static void second_level_work_check(unsigned long now)
 
     for_each_possible_cpu(cpu)
     {
-        if (cpu && !cpu_online(cpu))
+        if (cpu && likely(!cpu_online(cpu)))
         {
             cpu_up(cpu);
             pr_info("Hotplug: cpu%d is up - medium load\n", cpu);
             break;
         }
     }
-
-    if (stats.online_cpus == 1)
-        scale_interactive_tunables(50, 99, 30, 20);
- 
-    else if (stats.online_cpus == 3) 
-        scale_interactive_tunables(0, 80, 10, 80);
 
     stats.time_stamp = now;
 }
@@ -141,7 +133,7 @@ static void third_level_work_check(unsigned int load, unsigned long now)
             }
         }
 
-        scale_interactive_tunables(15, 99, 25, 40);
+        scale_interactive_tunables(50, 99, 30, 20);
     }
 
     else if (load <= third_level)
@@ -155,6 +147,9 @@ static void third_level_work_check(unsigned int load, unsigned long now)
                 break;
             }
         }
+
+        if (stats.online_cpus < 3)
+            scale_interactive_tunables(50, 99, 30, 20);
     }
 
     stats.time_stamp = now;
@@ -204,6 +199,7 @@ static void decide_hotplug_func(struct work_struct *work)
 
     if (load >= first_level)
     {
+        scale_interactive_tunables(0, 80, 10, 80);
         first_level_work_check(now);
         queue_delayed_work_on(0, wq, &decide_hotplug, msecs_to_jiffies(HZ));
         return;
@@ -213,7 +209,12 @@ static void decide_hotplug_func(struct work_struct *work)
     else if (is_touched)
     {   
         if (now >= touch_off_time + SEC_THRESHOLD)
+        {
+            /* only call scale function if dynamic_scaling is true */
+            if (get_dynamic_scaling())
+                scale_min_sample_time(20);
             is_touched = false;
+        }
 
         else if (stats.online_cpus < stats.cores_on_touch)
         {
@@ -242,8 +243,8 @@ static void decide_hotplug_func(struct work_struct *work)
 static void mako_hotplug_early_suspend(struct early_suspend *handler)
 {	 
     /* cancel the hotplug work when the screen is off and flush the WQ */
-    flush_workqueue(wq);
     cancel_delayed_work_sync(&decide_hotplug);
+    flush_workqueue(wq);
     pr_info("Early Suspend stopping Hotplug work...\n");
     
     third_level_work_check(0, ktime_to_ms(ktime_get()));
@@ -257,15 +258,8 @@ static void mako_hotplug_early_suspend(struct early_suspend *handler)
 
 static void mako_hotplug_late_resume(struct early_suspend *handler)
 {    
-    int i;
-
     /* online all cores when the screen goes online */
     first_level_work_check(ktime_to_ms(ktime_get()));
-
-    /* feed the buffer with fake load so that it doesn't average from low
-       values and derp the UI interactivity after screening on */
-    for (i = 0; i < HISTORY_SIZE; i++)
-        load_history[i] = 100;
 
     /* restore max frequency */
     msm_cpufreq_set_freq_limits(0, MSM_CPUFREQ_NO_LIMIT, MSM_CPUFREQ_NO_LIMIT);
